9918d30a3afb772016ba9a7d5645dfec6206d700
LC1A_03_KokiAbe <k020g1182@g.neec.ac.jp>
2022-07-08 13:17:08 +0900

    02_01

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow();
+	win->CreateGameWindow("LE2D_01_アベ_コウキ_AL3");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
